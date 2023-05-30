d26fc9452b0442e6fb49e897503b70958b77ed75
Matthew Sun <sun3matthew@gmail.com>
2023-05-30 03:11:49 -0700

    light wrok

--- Active/Unbricked/main.asm
+++ Active/Unbricked/main.asm
@@ -5,11 +5,21 @@ SECTION "Header", ROM0[$100]
 jp EntryPoint ; true entry point: at $100
 
 ds $150 - @, 0 ; Make room for the header
+; gets filled by rgbfix
 
 EntryPoint:
 	; Shut down audio circuitry
 	ld a, 0
 	ld [rNR52], a
+
+WaitVBlank:
+    ld a, [rLY]
+    cp 144
+    jp c, WaitVBlank
+
+    ; Turn the LCD off
+    ld a, 0
+    ld [rLCDC], a
 
 Done:
 	jp Done
